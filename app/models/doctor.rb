27bb1f97b5e0765a2bf05b8d45189bc4b9994745
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-12 00:02:06 +0000

    Automatically backed up by Learn

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,10 @@
 class Doctor < ApplicationRecord
   has_many :appointments
   has_many :patients, through: :appointments
+
+
+  def app_time
+    self.appointment_datetime.strftime("%B %d, %Y at %H:%M")
+  end
+
 end
